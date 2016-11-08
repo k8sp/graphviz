067cdbec89140e42f73822e3ea7cf19845a451cc
Yi Wang <yiwang01@baidu.com>
2016-11-08 13:08:02 -0800

    Make graphviz.go a usable program

--- graphviz.go
+++ graphviz.go
@@ -1,60 +1,73 @@
+// graphviz is an HTTP server which calls GraphViz's dot command to
+// visualize a .dot file given in the `dot` parameter.  For example,
+// we can run this program as follows
+//
+//   go run graphviz.go
+//
+// and access it from the Web browser with URL:
+//
+//   http://localhost:8080/?dot=https://gist.githubusercontent.com/wangkuiyi/c4e0015211dd1b9bde2e20455a6cd38e/raw/4d5ec099f98a5f326cf6f108bcf510cadba1a0b4/ci-arch.dot
+//
+// then the visualization of ci-arch.dot should appear in the browser window.
+//
 package main
 
 import (
+	"crypto/md5"
+	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"path"
+
+	"github.com/topicai/candy"
 )
 
 func main() {
-	http.HandleFunc("/", makeSafeHandler(func(w http.ResponseWriter, r *http.Request) {
-		source := "https:/" + r.URL.Path
-		resp, e := http.Get(source)
-		if e != nil {
-			log.Panicf("Cannot retreive source %s: %v", source, e)
-		}
-		defer resp.Body.Close()
+	dir := flag.String("dir", "/tmp", "The cache directory")
+	addr := flag.String("addr", ":8080", "The listening address")
+	flag.Parse()
 
-		cmd := exec.Command("dot", "-Tpng")
-		w.Header().Set("Content-Type", "image/png")
+	http.HandleFunc("/",
+		makeSafeHandler(func(w http.ResponseWriter, r *http.Request) {
+			if source := r.FormValue("dot"); len(source) > 0 {
+				dot, e := candy.HTTPGet(source, 0)
+				candy.Must(e)
 
-		if stdin, e := cmd.StdinPipe(); e != nil {
-			log.Panicf("Cannot pipe to stdin: %v", e)
-		} else {
-			go func() {
-				if _, e := io.Copy(stdin, resp.Body); e != nil {
-					log.Panicf("Failed copying source to graphviz: %v", e)
+				id := fmt.Sprintf("%015x", md5.Sum(dot))
+				dotFile := path.Join(*dir, id) + ".dot"
+				pngFile := path.Join(*dir, id) + ".png"
+
+				if _, e := os.Stat(pngFile); os.IsNotExist(e) {
+					// TODO(yi): Here we adopt an unlimted-size disk-based cache.  We should
+					// either introduce LRU and limit the size, or periodically clean it.
+					log.Printf("Update cache for %s", pngFile)
+					candy.Must(ioutil.WriteFile(dotFile, dot, 0755))
+					png, e := exec.Command("dot", "-Tpng", dotFile).Output()
+					candy.Must(e)
+					candy.Must(ioutil.WriteFile(pngFile, png, 0755))
 				}
-			}()
-		}
 
-		if stdout, e := cmd.StdoutPipe(); e != nil {
-			log.Panicf("Cannot read stdout: %v", e)
-		} else {
-			go func() {
-				if _, e := io.Copy(w, stdout); e != nil {
-					log.Panicf("Failed delivering output: %v", e)
-				}
-			}()
-		}
+				png, e := ioutil.ReadFile(pngFile)
+				candy.Must(e)
+				_, e = w.Write(png)
+				candy.Must(e)
+			}
+		}))
 
-		if e := cmd.Start(); e != nil {
-			log.Panicf("%v", e)
-		}
-	}))
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	candy.Must(http.ListenAndServe(*addr, nil))
 }
 
-func makeSafeHandler(in http.HandlerFunc) http.HandlerFunc {
+func makeSafeHandler(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if e := recover(); e != nil {
 				http.Error(w, fmt.Sprint(e), http.StatusInternalServerError)
 			}
 		}()
-		in(w, r)
+		h(w, r)
 	}
 }
